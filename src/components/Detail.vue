ffec7a6151982df676d44169ed5fd721de85d8f8
wclimb <875246904@qq.com>
2017-12-01 14:18:05 +0800

    add github link

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -22,6 +22,7 @@
                     </div>
                 </div>        
             </div>
+            <a target="_blank" href="https://github.com/wclimb/vue-video"><svg class="github" fill="#FFF" height="32" version="1.1" viewBox="0 0 16 16" width="32"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path></svg></a>
         </header>
         <section class="video_txt">
             <div class="video_txt_wrap">
@@ -92,10 +93,16 @@
             </ul>
         </section>
         <section class="page">
-            <div @click="goPage(1)" v-if="Math.ceil(pageNeedComments.length / 5) > 1">首页</div>
+            <div @click="goPage(1)" v-if="(commentsPageLength > 1) && page > 1">首页</div>
             <div @click="prevPage()" v-if="page >= 2">上一页</div>
-            <div @click="nextPage()" v-if="page < Math.ceil(pageNeedComments.length / 5)" >下一页</div>
-            <div @click="goPage(Math.ceil(pageNeedComments.length / 5))" v-if="page < Math.ceil(pageNeedComments.length / 5)">尾页</div>
+            <div @click="nextPage()" v-if="page < commentsPageLength" >下一页</div>
+            <div @click="goPage(commentsPageLength)" v-if="page < commentsPageLength">尾页</div>
+            <template v-if="commentsPageLength >= 1">
+                <div class="pageNum">{{page}}/{{commentsPageLength}}页</div>
+            </template>
+            <template v-else>
+                <div class="pageNum">暂时没有评论...</div>
+            </template>
         </section>
         <alert-dialog v-if="dialogShow" :icon="tipsImg" :aniDialog="aniDialog"  :dialogTxt="dialogTxt"></alert-dialog>
     </section>
@@ -150,6 +157,9 @@ export default {
         },
         avator(){ 
           return localStorage.getItem('avator');
+        },
+        commentsPageLength(){
+            return Math.ceil(this.pageNeedComments.length / 5)
         }
     },
     mounted(){
@@ -326,21 +336,4 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 @import 'src/style/detail'; 
-.page {
-    margin: 0.2rem 0;
-    display: flex;
-    justify-content: center;
-}
-.page div{
-    display: inline;
-    background: #4ebf60;
-    color: #fff;
-    text-align: center;
-    margin:0 0.05rem;
-    border-radius:5px;
-    padding:0.08rem 0.14rem;
-}
-.page div:active{
-    background: #89e498;
-}
 </style>
