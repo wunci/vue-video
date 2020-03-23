413be64d881c2e5b3cd98bb203f80eef7c97b89e
wclimb <875246904@qq.com>
2020-03-23 21:46:30 +0800

    delete: comment

--- src/main.js
+++ src/main.js
@@ -1,25 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import router from './router';
-import store from './store';
-import './base/rem';
-import VueLazyload from 'vue-lazyload';
-import Toast from './components/common/Toast.js';
-import promise from 'es6-promise';
-import Img from './assets/imgLoading.svg';
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import store from "./store";
+import "./base/rem";
+import VueLazyload from "vue-lazyload";
+import Toast from "./components/common/Toast.js";
+import promise from "es6-promise";
+import Img from "./assets/imgLoading.svg";
 promise.polyfill();
 Vue.use(Toast);
 Vue.use(VueLazyload, {
-  loading: Img,
+  loading: Img
 });
 Vue.config.productionTip = false;
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
-  template: '<App/>',
-  components: { App },
+  template: "<App/>",
+  components: { App }
 });
